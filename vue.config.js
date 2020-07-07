6e8580f3c980b57c419d07116b9e46e4cb62d2c6
langnang <33144853+langnang@users.noreply.github.com>
2020-07-07 09:44:08 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,13 @@ module.exports = {
     outputDir: 'dist',
     publicPath: process.env.NODE_ENV === 'production'
         ? '/vue-demo/'
-        : '/'
+        : '/',
+    chainWebpack: config => {
+        config
+            .plugin('html')
+            .tap(args => {
+                args[0].title = 'Vue Demo'
+                return args
+            })
+    }
 }
