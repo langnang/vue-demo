50a18e8c2453a578b72c91e01ac019dda5ee55bf
langlang.chen <langlang.chen@advantech.com.cn>
2020-10-18 14:44:37 +0800

    Update fontawesome.js

--- src/plugins/fontawesome.js
+++ src/plugins/fontawesome.js
@@ -4,8 +4,11 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText }
+    from '@fortawesome/vue-fontawesome'
 
 library.add(fas, far, fab)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.component('font-awesome-layers', FontAwesomeLayers)
+Vue.component('font-awesome-layers-text', FontAwesomeLayersText)
