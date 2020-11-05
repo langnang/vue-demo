e8dc276e4935a00ab5a70ac1281dffdd17f060c3
langlang.chen <langlang.chen@advantech.com.cn>
2020-11-06 00:39:29 +0800

    latest

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,13 @@
 module.exports = {
-    outputDir: 'dist',
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/vue-demo/'
-        : '/',
-    devServer: {
-        port: 19103,// 自定义端口
-    },
-    chainWebpack: config => {
-        config
-            .plugin('html')
-            .tap(args => {
-                args[0].title = 'Vue Demo'// 自定义标题
-                return args
-            })
-    }
-}
+  outputDir: "dist",
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-template/" : "/",
+  devServer: {
+    port: 9100, // 自定义端口
+  },
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].title = "Vue Template"; // 自定义标题
+      return args;
+    });
+  },
+};
