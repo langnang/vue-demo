58d23ccb4870515f400e22ee105e3df8b55c29f1
langlang.chen <langlang.chen@advantech.com.cn>
2020-11-06 01:11:27 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,25 +1,21 @@
-import Vue from 'vue'
-import './plugins/axios'
-import './plugins/fontawesome'
-import App from './App.vue'
-import router from './routes'
-import store from './store'
-import './plugins/element.js'
-import 'particles.js';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./routes";
+import store from "./store";
+import "./plugins/axios";
+import "./plugins/element";
+import "./plugins/fontawesome";
+import "./plugins/vue-wechat-title";
+import "particles.js";
 
-Vue.config.productionTip = false
-Vue.use(require('vue-wechat-title'))
+import particlesData from "./particles.json";
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
 
-window.axios
-  .get("https://langnang.github.io/src/json/particles.js/index.json")
-  .then(res => {
-    if (res.status === 200) {
-      window.particlesJS('app', res.data);
-    }
-  })
+window.particlesJS("app", particlesData);
