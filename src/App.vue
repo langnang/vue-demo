7781f2fd40083a9971bd55689fd31963342fd4fe
langlang.chen <langlang.chen@advantech.com.cn>
2020-11-06 01:00:34 +0800

    vue-wechat-title

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,40 @@
 <template>
-	<div id="app">
-		<img alt="Vue logo" src="./assets/logo.png" />
-		<HelloWorld msg="Welcome to Your Vue.js App" />
-		<router-view></router-view>
-	</div>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <router-view v-wechat-title="$route.meta.title"></router-view>
+  </div>
 </template>
 
 <script>
-	import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
-	export default {
-		name: "App",
-		components: {
-			HelloWorld,
-		},
-	};
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
-	html,
-	body {
-		margin: 0;
-		padding: 0;
-	}
-	#app {
-		font-family: Avenir, Helvetica, Arial, sans-serif;
-		-webkit-font-smoothing: antialiased;
-		-moz-osx-font-smoothing: grayscale;
-		text-align: center;
-		color: #2c3e50;
-		margin-top: 60px;
-	}
-	.particles-js-canvas-el {
-		position: fixed;
-		top: 0;
-		left: 0;
-		z-index: -1;
-	}
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.particles-js-canvas-el {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: -1;
+}
 </style>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,32 +1,49 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-import HomeRoute from './../views/home.vue'
-import ErrorRoute from './../views/error.vue'
-import DynamicRoute from './../views/dynamic.vue';
+import HomeRoute from "./../views/home.vue";
+import ErrorRoute from "./../views/error.vue";
+import DynamicRoute from "./../views/dynamic.vue";
 
-import VueAwesomeRoute from './../views/vue-awesome.vue';
-
+import VueAwesomeRoute from "./../views/vue-awesome.vue";
 
 Vue.use(VueRouter);
-export default new VueRouter({
-    mode: "hash",
-    routes: [
-        {
-            path: "/",
-            component: HomeRoute,
-        },
-        {
-            path: "/vue-awesome",
-            component: VueAwesomeRoute,
-        },
-        {
-            path: "/dynamic/:id",
-            component: DynamicRoute,
-        },
-        {
-            path: "*",
-            component: ErrorRoute,
-        }
-    ]
-})
+const router = new VueRouter({
+  mode: "hash",
+  routes: [
+    {
+      path: "/",
+      component: HomeRoute,
+      meta: {
+        title: "Home",
+      },
+    },
+    {
+      path: "/vue-awesome",
+      component: VueAwesomeRoute,
+      meta: {
+        title: "VueAwesome",
+      },
+    },
+    {
+      path: "/dynamic/:id",
+      component: DynamicRoute,
+      meta: {
+        title: "Dynamic",
+      },
+    },
+    {
+      path: "*",
+      component: ErrorRoute,
+      meta: {
+        title: "Error",
+      },
+    },
+  ],
+});
+router.beforeEach((to, from, next) => {
+  to.meta.title += " | Vue Template";
+  next();
+});
+
+export default router;
